5e5c4d0ef50f90389a65eb4c6ddafd698b2e152e
Seth Baughman <sbaughman1@me.com>
2017-01-08 10:28:47 -0500

    Refactor resourceries controller

--- app/controllers/resourceries_controller.rb
+++ app/controllers/resourceries_controller.rb
@@ -4,29 +4,25 @@ class ResourceriesController < ApplicationController
   before_action :set_prep, only:           [:index, :show]
 
   def index
-    @resourceries = @prep.resourceries.order(created_at: :desc)
-    @resources    = @resourceries.map { |rsrcry| rsrcry.resource }
-    if current_user == @prep.coach
-      @unshared_resources = current_user.resources - @resources
-    end
+    @resources = @prep.resourceries.order(created_at: :desc).map { |rsrcry| rsrcry.resource }
+    @unshared_resources = current_user.resources - @resources if current_user.coach
   end
 
   def new
-    @resource   = Resource.find(params[:resource])
+    @resource   = Resource.find(get_resource_id)
     @resourcery = Resourcery.new
     @preps      = Prep.where(coach_id: current_user.id)
   end
 
   def create
+    get_prep_ids.each do |id|
+      Resourcery.create(resource_id: get_resource_id, prep_id: id) unless id.blank?
+    end
+
     if params[:resourcery]
-      prep_ids = params[:resourcery][:prep_id]
-      prep_ids.each do |id|
-        Resourcery.create(resource_id: params[:resourcery][:resource_id], prep_id: id) unless id.blank?
-      end
       flash[:success] = "Resource shared"
       redirect_to current_user
     else
-      Resourcery.create(resource_id: params[:resource_id], prep_id: params[:prep_id])
       redirect_back(fallback_location: root_path)
     end
   end
@@ -44,15 +40,17 @@ class ResourceriesController < ApplicationController
     params.require(:resourcery).permit(:resource_id, :prep_id)
   end
 
+  def get_resource_id
+    params[:resource_id] || params[:resourcery] && params[:resourcery][:resource_id]
+  end
+
+  def get_prep_ids
+    (params[:resourcery] && params[:resourcery][:prep_id]) || [params[:prep_id]]
+  end
+
   def user_owns_resource
-    if params[:resourcery]
-      resource = Resource.find(params[:resourcery][:resource_id])
-    elsif params[:resource_id]
-      resource = Resource.find(params[:resource_id])
-    else
-      resourcery = Resourcery.find(params[:id])
-      resource = resourcery.resource
-    end
+    resource_id = get_resource_id
+    resource = resource_id ? Resource.find(resource_id) : Resourcery.find(params[:id]).resource
     if current_user != resource.user
       flash[:alert]  = "You don't have permission to do that"
       redirect_back(fallback_location: root_path)
